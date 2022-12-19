e5dce70619a7a3394279e81776b633ccd2149678
felipe-almeida-gomes <falmeidagomes13@gmail.com>
2022-12-18 21:13:21 -0300

    Primeiro commit

---
+++ script/login.js
@@ -0,0 +1,21 @@
+const loginName = document.querySelector('.login-name')
+const loginButton = document.querySelector('.login-button')
+const form = document.querySelector('form')
+
+const loginCheck = ({ target }) => {
+	if (target.value.length < 3) {
+		loginButton.setAttribute('disabled', '')
+		return
+	}
+	loginButton.removeAttribute('disabled')
+}
+
+const loginSubmit = (event) => {
+    event.preventDefault()
+
+    localStorage.setItem('player', loginName.value)
+    window.location = 'pages/game.html'
+}
+loginName.value = localStorage.player
+loginName.addEventListener('input', loginCheck)
+form.addEventListener('submit', loginSubmit)
