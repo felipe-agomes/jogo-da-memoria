fd6bdff2abf6ce3f574dfb9b38bff787ed6c303e
felipe-almeida-gomes <falmeidagomes13@gmail.com>
2022-12-23 21:54:30 -0300

    Finalizado o grid e introduzindo contadores

--- script/game.js
+++ script/game.js
@@ -1,8 +1,15 @@
 const card = document.querySelector('.card')
+const metter = document.querySelector('.meter')
+const timer = document.querySelector('.timer')
+let endGame = false
+
+let firstCard = ''
+let secondCard = ''
+let count = 0
 
 const startGame = () => {
-    const characters = [
-        'summer',
+	const characters = [
+		'summer',
 		'scroopy',
 		'rick',
 		'pickle-rick',
@@ -10,7 +17,7 @@ const startGame = () => {
 		'morty',
 		'meeseeks',
 		'jessica',
-        
+
 		'jerry',
 		'beth',
 	]
@@ -20,35 +27,98 @@ const startGame = () => {
 }
 
 const createElement = (tag, className) => {
-    const element = document.createElement(tag)
+	const element = document.createElement(tag)
 	element.className = className
 	return element
 }
 
 const createCard = (characters) => {
-    const cardGrid = document.querySelector('.card-grid')
-    
+	const cardGrid = document.querySelector('.card-grid')
+
 	characters.forEach((character) => {
-        const card = createElement('div', 'card')
+		const card = createElement('div', 'card')
+		card.setAttribute('character', 'beth')
 		cardGrid.appendChild(card)
-        
+
 		const front = createElement('div', 'face front')
-		front.style.backgroundImage = `url(../images/${character}.png)`
+		// front.style.backgroundImage = `url(../images/${character}.png)`
+		front.style.backgroundImage = `url(../images/beth.png)`
 		card.appendChild(front)
-        
+
 		const back = createElement('div', 'face back')
 		back.style.backgroundImage = 'url(../images/back.png)'
 		card.appendChild(back)
 
-        card.addEventListener('click', revealCard)
+		// card.addEventListener('click', revealCard)
+		card.addEventListener('click', revealCard)
 	})
 }
 
+const checkCard = () => {
+	const firstCharacter = firstCard.getAttribute('character')
+	const secondCharacter = secondCard.getAttribute('character')
+	
+	if (firstCharacter == secondCharacter) {
+		firstCard = ''
+		secondCard = ''
+		return
+	} else {
+		setTimeout(() => {
+			firstCard.classList.remove('active')
+			secondCard.classList.remove('active')
+			
+			firstCard = ''
+			secondCard = ''
+		}, 500)
+	}
+}
 
-            /* working */
+const meterCount = () => {
+	if (count < 10) {
+		metter.innerHTML = '0' + count
+	} else {
+		metter.innerHTML = count
+	}
+}
 
-const revealCard = ({target}) => {
-    console.log(target);
+const revealCard = ({ target }) => {
+	if (target.parentNode.className.includes('active')) {
+		return
+	}
+	
+	if (firstCard == '') {
+		target.parentNode.classList.add('active')
+		firstCard = target.parentNode
+	} else if (secondCard == '') {
+		target.parentNode.classList.add('active')
+		secondCard = target.parentNode
+		count++
+		timerCount()
+		meterCount()
+		
+		checkCard()
+	}
+	
+	if (count == 10) {
+		endGame = true
+		alert('You won!!')
+	}
 }
 
 startGame()
+
+/* working */
+
+const timerCount = () => {
+	const currentTime = 0
+	while (endGame == false) {
+		setInterval(() => {
+			if (count < 10) {
+				timer.innerHTML = '0' + currentTime
+			} else {
+				timer.innerHTML = currentTime
+			}
+			currentTime++
+		}, 1000)
+	}
+}
